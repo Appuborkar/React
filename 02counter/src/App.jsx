2a246f732458e96f2425434e7a75334c529b566a
Apurv <apurvborkar56@gmail.com>
2025-05-12 16:28:15 +0530

    d

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,19 +3,18 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
-  let counter=5;
-  const addValue=()=>{
-  counter=+1;
-  console.log("value added",counter);
-}
+  let counter = 5;
+  const addValue = () => {
+    counter = +1;
+    console.log("value added", counter);
+  } 
 
-  return (    
+  return (
     <>
- 
-<h1>Chai aur react</h1>
-<h2>Counter Value:{counter}</h2>
-<button onClick={addValue}>add value</button><br></br>
-<button>remove value</button>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value:{counter}</h2>
+      <button onClick={addValue}>add value</button><br></br>
+      <button>remove value</button>
     </>
   )
 }
