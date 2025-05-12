992105d0581d794a856afbe916f503691309e9ea
Apurv <apurvborkar56@gmail.com>
2025-05-12 17:11:00 +0530

    hooks started

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,20 +1,33 @@
+import { useState } from 'react';
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
-  let counter = 5;
+ let [counter,setCounter] =useState(5)
+  // let counter = 5;
   const addValue = () => {
-    counter = +1;
-    console.log("value added", counter);
+    // counter = counter+1;
+    if(counter<=19){
+    setCounter(counter+1)}
+    else{
+      alert("cannot add the number greater than 20")
+    }
   } 
+  const removeValue=()=>{
+    // counter=counter-1
+    if(counter>=1){
+    setCounter(counter-1)
+  }
+  else{
+    alert("cannot delete the number less than 0")
+  }
+  }
 
   return (
     <>
       <h1>Chai aur react</h1>
       <h2>Counter Value:{counter}</h2>
-      <button onClick={addValue}>add value</button><br></br>
-      <button>remove value</button>
+      <button onClick={addValue}>add value</button><br/>
+      <button onClick={removeValue}>remove value</button>
     </>
   )
 }
