967542b020842b1fc112d989200f2f405088f043
Apurv <apurvborkar56@gmail.com>
2025-05-14 12:18:25 +0530

    learned props and tailwind injection

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,22 +4,22 @@ import "./App.css";
 function App() {
   const [counter, setCounter] = useState(1);
   const addValue = () => {
-    if (counter <= 19) {
+  
 
 
-      setCounter(counter + 1)
-    }
-    else {
-      alert("you cannot add the number greater than 20")
-    }
+      setCounter(prevCounter => prevCounter+ 1)
+      setCounter(prevCounter => prevCounter+ 1)
+      setCounter(prevCounter => prevCounter+ 1)
+      setCounter(prevCounter => prevCounter+ 1)
+      setCounter(prevCounter => prevCounter+ 1)
+
+   
   }
   const removeValue = () => {
 
-    if (counter >= 1) {
+    
       setCounter(counter - 1)
-    } else {
-      alert("you cannot remove the number less than 0")
-    }
+    
   }
   return (
     <><h1>Hello Apurv{counter}</h1>
