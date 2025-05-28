952a147b0407aa547f86af9117d1960b4c515360
Apurv <apurvborkar56@gmail.com>
2025-05-28 16:16:48 +0530

    done

--- 07reactrouter/src/index.js
+++ 07reactrouter/src/index.js
@@ -4,7 +4,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import Layout from './Layout';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
-import Github,{githubLoader} from './components/Github/Github';
+import Github, { githubLoader } from './components/Github/Github';
 import Home from './components/Home/Home';
 import User from './components/User/User';
 import './index.css';
